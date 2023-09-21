e601772d0fb6cb5fe85cccfab3e6677d53c27755
TheVy <ntvy2k@gmail.com>
2023-09-21 15:46:41 +0700

    add todo

--- vcard/src/rfc6350/properties.rs
+++ vcard/src/rfc6350/properties.rs
@@ -1,3 +1,40 @@
+/*
+TODO:
+-----------------
+- PHOTO
+- BDAY
+- ANNIVERSARY
+- GENDER
+- ADR
+- TEL
+- EMAIL
+- IMPP
+- LANG
+- TZ
+-----------------
+- GEO
+-----------------
+- TITLE
+- ROLE
+- LOGO
+- ORG
+- MEMBER
+- RELATED
+------------------
+- CATEGORIES
+- NOTE
+- PRODID
+- REV
+- SOUND
+- UID
+- CLIENTPIDMAP
+- URL
+------------------
+- FBURL
+- CALADRURI
+- CALURI
+*/
 pub mod name;
+pub mod photo;
 
 pub use name::{FullName, Name, NickName};
