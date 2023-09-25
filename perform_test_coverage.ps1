56269eedbd3887bc5a5f549bf3bcc156644a7284
TheVy <ntvy2k@gmail.com>
2023-09-25 15:23:52 +0700

    minor fix

--- perform_test_coverage.ps1
+++ perform_test_coverage.ps1
@@ -1,6 +1,15 @@
+# Setup
 $env:RUSTFLAGS="-C instrument-coverage"
+
+# Perform
 cargo clean
 cargo build
 cargo test --tests
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
+
+# Cleanup
+rm *.profraw
 rm **/*.profraw
+
+# Display
+echo "Output: ./target/debug/coverage/"
